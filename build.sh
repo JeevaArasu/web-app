f5380db584bc4000a444c2f7023a1684e7b73de4
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-06 12:15:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t capstone .
-
-docker run -d --name react-app -p 80:80 capstone
+docker run -d -it --name react-app -p 80:80 capstone
+docker tag capstone  jeevaarasu/react-app
