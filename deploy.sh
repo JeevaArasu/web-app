29b5654afa733cc2d8d250e8bf5eda50581a3909
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-06 13:17:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker login -u $DOCKER_USERNAME  -p $DOCKER_PASS
 if [[$GIT_BRANCH == "origin/main"]]; then
 docker tag reactapp jeevaarasu/capstone:prod
 docker push jeevaarasu/prod
-elif [[$GIT_BRANCH== "dev"]]; then
+elif [[$GIT_BRANCH== "demo"]]; then
 docker tag reactapp jeevaarasu/capstone:dev
 docker push jeevaarasu/dev
 fi
